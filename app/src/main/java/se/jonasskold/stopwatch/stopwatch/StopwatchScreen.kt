d0dbf8716e9a0614f586bbf79ead7e9de8a124a1
Jonas Sköld <jonas.skold@bontouch.com>
2020-10-01 20:57:06 +0200

    Style time of stopwatch

--- app/src/main/java/se/jonasskold/stopwatch/stopwatch/StopwatchScreen.kt
+++ app/src/main/java/se/jonasskold/stopwatch/stopwatch/StopwatchScreen.kt
@@ -1,11 +1,15 @@
 package se.jonasskold.stopwatch.stopwatch
 
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.FirstBaseline
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.viewModel
 import androidx.ui.tooling.preview.Preview
 import kotlinx.coroutines.ObsoleteCoroutinesApi
@@ -49,11 +53,38 @@ fun StopwatchButtons(
 
 @Composable
 fun Time(time: Long) {
-    val hours: Long = remember(time) { time / 360000 }
-    val minutes: Long = remember(time) { (time % 360000) / 60000 }
-    val seconds: Long = remember(time) { (time % 60000) / 1000 }
-    val millis: Long = remember(time) { time % 1000 }
-    Text(text = "$hours:$minutes:$seconds.$millis")
+    val hours: String = remember(time) {
+        val h = time / 3600000
+        if (h == 0L) "" else "$h:"
+    }
+    val minutes: String = remember(time) {
+        val m = (time % 3600000) / 60000
+        if (m == 0L) "" else "$m:"
+    }
+    val seconds: String = remember(time) { "${(time % 60000) / 1000}" }
+    val millis: String = remember(time) {
+        val ms = (time % 1000) / 10
+        if (ms < 10) "0$ms" else "$ms"
+    }
+    Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+                text = "$hours$minutes$seconds",
+                style = MaterialTheme.typography.h1,
+                modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .alignWithSiblings(FirstBaseline)
+        )
+        Text(
+                text = millis,
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier
+                        .alignWithSiblings(FirstBaseline)
+        )
+
+    }
 }
 
 @Preview
@@ -61,7 +92,7 @@ fun Time(time: Long) {
 fun preview() {
     StopwatchTheme {
         Surface {
-            Time(time = 1500)
+            Time(time = 83450)
         }
     }
 }
