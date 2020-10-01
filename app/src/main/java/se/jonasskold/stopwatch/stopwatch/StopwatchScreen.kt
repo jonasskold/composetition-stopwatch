bd5b30cdfe5f49822464b7f178457173e0996750
Jonas Sköld <jonas.skold@bontouch.com>
2020-10-01 21:20:52 +0200

    Style buttons

--- app/src/main/java/se/jonasskold/stopwatch/stopwatch/StopwatchScreen.kt
+++ app/src/main/java/se/jonasskold/stopwatch/stopwatch/StopwatchScreen.kt
@@ -9,12 +9,14 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.viewModel
 import androidx.ui.tooling.preview.Preview
 import kotlinx.coroutines.ObsoleteCoroutinesApi
-import se.jonasskold.stopwatch.model.Stopwatch
 import se.jonasskold.stopwatch.ui.StopwatchTheme
+import se.jonasskold.stopwatch.ui.green
+import se.jonasskold.stopwatch.ui.red
 
 @ObsoleteCoroutinesApi
 @Composable
@@ -26,7 +28,7 @@ fun StopwatchScreen() {
             Column {
                 Time(time = viewModel.stopwatch.totalAt(viewModel.now))
                 StopwatchButtons(
-                        stopwatch = viewModel.stopwatch,
+                        isRunning = viewModel.stopwatch.isRunning,
                         onClickResume = { viewModel.resume() },
                         onClickStop = { viewModel.stop() },
                         onClickReset = { viewModel.reset() }
@@ -38,16 +40,34 @@ fun StopwatchScreen() {
 
 @Composable
 fun StopwatchButtons(
-        stopwatch: Stopwatch,
-        onClickResume: () -> Unit,
-        onClickStop: () -> Unit,
-        onClickReset: () -> Unit
+        isRunning: Boolean = false,
+        onClickResume: () -> Unit = {},
+        onClickStop: () -> Unit = {},
+        onClickReset: () -> Unit = {}
 ) {
-    Button(onClick = if (stopwatch.isRunning) onClickStop else onClickResume) {
-        Text(text = if (!stopwatch.isRunning) "Start" else "Stop")
-    }
-    Button(onClick = onClickReset) {
-        Text(text = "Reset")
+    Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+    ) {
+        Button(
+                onClick = if (isRunning) onClickStop else onClickResume,
+                modifier = Modifier
+                        .preferredWidth(128.dp)
+                        .padding(8.dp),
+                backgroundColor = if (isRunning) red else green
+        ) {
+            Text(
+                    text = if (!isRunning) "Start" else "Stop",
+                    color = Color.White)
+        }
+        Button(
+                onClick = onClickReset,
+                modifier = Modifier
+                        .preferredWidth(128.dp)
+                        .padding(8.dp)
+        ) {
+            Text(text = "Reset")
+        }
     }
 }
 
@@ -92,7 +112,10 @@ fun Time(time: Long) {
 fun preview() {
     StopwatchTheme {
         Surface {
-            Time(time = 83450)
+            Column {
+                Time(time = 83450)
+                StopwatchButtons()
+            }
         }
     }
 }
--- app/src/main/java/se/jonasskold/stopwatch/ui/Color.kt
+++ app/src/main/java/se/jonasskold/stopwatch/ui/Color.kt
@@ -2,7 +2,6 @@ package se.jonasskold.stopwatch.ui
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+val blue = Color(0xFF4792D8)
+val green = Color(0xFF6EBE49)
+val red = Color(0xFFC9544D)
